c448814619e31cf93212156f9709fee8350b74f9
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2017-10-24 14:46:26 +0700

    24.10

--- html/js/loading.js
+++ html/js/loading.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-// Masonry Grid
+
 var slideIndex = 0;
 showSlides();
 
@@ -17,27 +16,6 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-=======
-// Masonry Grid
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
->>>>>>> 6504b1ef78bde54105e855b97e86e4e285ad2953
